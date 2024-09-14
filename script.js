241f9aaa314226dc094462ec379e1fcb442bf5c6
W0lfabian <157919968+W0lfabian@users.noreply.github.com>
2024-09-14 15:01:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,56 @@
+// Slideshow obrázků v sekci Domů
 let slideIndex = 1;
 showSlides(slideIndex);
 
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
+// Funkce pro otevření modalu
+function openModal(n) {
+    document.getElementById("myModal").style.display = "block";
+    currentSlide(n);
 }
 
+// Funkce pro zavření modalu
 function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+    document.getElementById("myModal").style.display = "none";
 }
 
+// Funkce pro zobrazení předchozích a následujících snímků v modalu
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    currentSlide(slideIndex += n);
 }
 
+// Funkce pro nastavení aktuálního snímku
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
+// Funkce pro zobrazení snímků v modalu
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
 
-// Slideshow auto-rotace
+// Automatické přepínání snímků v sekci Domů
 let autoIndex = 0;
 carousel();
 
 function carousel() {
-  let x = document.getElementsByClassName("slide");
-  for (let i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  autoIndex++;
-  if (autoIndex > x.length) {autoIndex = 1}    
-  x[autoIndex-1].style.display = "block";  
-  setTimeout(carousel, 3000); // Změna každé 3 sekundy
+    const slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    autoIndex++;
+    if (autoIndex > slides.length) { autoIndex = 1 }
+    slides[autoIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000); // Změna každé 3 sekundy
 }
 
+// Funkce pro volání
 function callOrder() {
     window.location.href = 'tel:607102261';
 }
